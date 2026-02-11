7dd75418d0eb7e8cbddf2dc0d0ccff65d0679ca9
Omar <omaryesid.dev@gmail.com>
2026-02-11 16:51:04 -0500

    Rename DateTime property to CreatedAt in Product entity

--- PruebaTecnica/Migrations/20260211215004_FixCreatedAtNAme.Designer.cs
+++ PruebaTecnica/Migrations/20260211215004_FixCreatedAtNAme.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PruebaTecnica.Data.Context;
 
@@ -11,9 +12,11 @@ using PruebaTecnica.Data.Context;
 namespace PruebaTecnica.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260211215004_FixCreatedAtNAme")]
+    partial class FixCreatedAtNAme
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PruebaTecnica/Migrations/20260211215004_FixCreatedAtNAme.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PruebaTecnica.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCreatedAtNAme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Products",
+                newName: "CreateAt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreateAt",
+                table: "Products",
+                newName: "DateTime");
+        }
+    }
+}
