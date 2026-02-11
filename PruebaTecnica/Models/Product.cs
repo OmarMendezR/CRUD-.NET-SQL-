d01aef867c3f0d63a3a563c013a67c089ea0a04c
Omar <omaryesid.dev@gmail.com>
2026-02-11 15:18:51 -0500

    Agregar archivos de proyecto.

---
+++ PruebaTecnica/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace PruebaTecnica.Models
+{
+    public class Product // Entity Base Class
+    {
+        // colums of Data Base
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Pricce { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreateAt { get; set; } 
+
+    }
+}
